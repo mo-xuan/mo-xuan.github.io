b648abf95b4e0704e6e5aeb3501832953559f715
xuan.mo <xuan.mo7@outlook.com>
2023-08-23 15:23:36 +0800

    github io

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,4 @@
 export default defineNuxtConfig({
   devtools: {enabled: true},
-  modules: ['@nuxtjs/tailwindcss'],
-  app: {
-    baseURL: '/mo-xuan.github.io/'
-  }
+  modules: ['@nuxtjs/tailwindcss']
 })
